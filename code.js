4c496a1d497d8c1f15094b8b7668efc6fea443fe
Jonny Mortemore <jonathanmortemore@gmail.com>
2025-03-04 10:38:11 +1100

    Add button functionality
    Add buttons that change the grid size and also reset the grid on click

--- code.js
+++ code.js
@@ -1,11 +1,25 @@
 const container = document.querySelector('#grid-container')
 const CANVAS_SIZE = 900;
 
-createGrid(16);
+createGrid(100);
+
+document.querySelector('#change-grid-size').addEventListener('click', () => {
+    let gridSize = ""
+    do {
+        gridSize = parseInt(prompt("What size should the grid be?", 50));
+    } while (isNaN(gridSize));
+    container.innerHTML = "";
+    createGrid(gridSize); 
+});
+
+document.querySelector('#reset-grid').addEventListener('click', () => {
+    document.querySelectorAll('.grid-element').forEach((el) => {
+        el.style.backgroundColor = "";
+    })
+})
 
 function createGrid(gridElements) {
     const gridElementSize = `${CANVAS_SIZE / gridElements}px`
-    console.log(gridElementSize)
     for (let i = 0; i < gridElements * gridElements; i++) {
         el = document.createElement('div');
         el.classList.toggle('grid-element');
